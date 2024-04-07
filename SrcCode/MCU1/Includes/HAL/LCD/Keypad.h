e03238856d2f5b0f9e51149b8537480160ae66dc
NohaHaydak <138703093+NohaHaydak@users.noreply.github.com>
2024-04-07 17:42:08 +0200

    Update Keypad.h

--- SrcCode/MCU1/Includes/HAL/LCD/Keypad.h
+++ SrcCode/MCU1/Includes/HAL/LCD/Keypad.h
@@ -7,15 +7,16 @@
 
 #ifndef HAL_INCLUDES_KEYPAD_H_
 #define HAL_INCLUDES_KEYPAD_H_
+
 #include "../../MCAL/INCLUDES/GPIO.h"
-#define KEYPAD_COL1 0
-#define KEYPAD_COL2 1
-#define KEYPAD_COL3 2
-#define KEYPAD_COL4 3
-#define KEYPAD_ROW1 4
-#define KEYPAD_ROW2 5
-#define KEYPAD_ROW3 6
-#define KEYPAD_ROW4 7
+//#define KEYPAD_COL1 0
+#define KEYPAD_COL2 0
+#define KEYPAD_COL3 1
+#define KEYPAD_COL4 2
+#define KEYPAD_ROW1 3
+#define KEYPAD_ROW2 4
+#define KEYPAD_ROW3 5
+#define KEYPAD_ROW4 6
 
 
 #define CONNECTION_PU 1
@@ -24,6 +25,18 @@
 
 #define SWITCH_PRESSED 1
 #define SWITCH_NOT_PRESSED 0
+
+
+#define ROWS_NUM 4
+#define COLS_NUM 3
+
+#define  NUM_OF_PINS 7
+/*
+typedef struct
+{
+	u8 col;
+	u8 row;
+}KeypadPost_t;*/
 
 typedef enum
 {
