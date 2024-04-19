4264e60af853668947b6417d60a82787bdb83d6a
NadaKazamel <nada.mk.174@gmail.com>
2024-04-19 21:34:37 +0200

    modification in keypad src file is done

--- SrcCode/MCU1/src/HAL/KEYPAD/Keypad.c
+++ SrcCode/MCU1/src/HAL/KEYPAD/Keypad.c
@@ -12,9 +12,8 @@
 
 extern const KEYPAD_t KEYPAD_Configuration[NUM_OF_PINS];
 extern const u8 KEYPAD_Keys[ROWS_NUM][COLS_NUM];
-u8 G_col=0;
-u8 G_row=0;
 u8 G_swState=0;
+u8 G_newPressedKey=0;
 //KeypadPost_t KEYPAD_postion;
 
 void KEYPAD_Init(void)
@@ -57,6 +56,25 @@ void KEYPAD_Init(void)
 KEYPAD_enuErrorStatus_t KEYPAD_voidGetPressedKey(u8 * Copy_pu8KeyValue)
 {
 	KEYPAD_enuErrorStatus_t KEYPAD_enuErrorStatus=KEYPAD_StatusOk;
+	if(Copy_pu8KeyValue == NULL )
+	{
+		KEYPAD_enuErrorStatus=KEYPAD_StatusNOk;
+	}
+	if(G_newPressedKey==1&&G_swState!=0)
+	{
+		* Copy_pu8KeyValue=G_swState;
+		G_newPressedKey=0;
+	}
+	else
+	{
+		* Copy_pu8KeyValue=0;
+	}
+		
+	return KEYPAD_enuErrorStatus;
+}
+/*KEYPAD_enuErrorStatus_t KEYPAD_voidGetPressedKey(u8 * Copy_pu8KeyValue)
+{
+	KEYPAD_enuErrorStatus_t KEYPAD_enuErrorStatus=KEYPAD_StatusOk;
 	if(Copy_pu8KeyValue == NULL)
 	{
 		KEYPAD_enuErrorStatus=KEYPAD_StatusNOk;
@@ -67,66 +85,46 @@ KEYPAD_enuErrorStatus_t KEYPAD_voidGetPressedKey(u8 * Copy_pu8KeyValue)
 	}
 	return KEYPAD_enuErrorStatus;
 }
-
-//each 5ms
+*/
 void KEYPADGetKey_Runnable(void)
 
 {
 	u8 Loc_swcurrent=0;
-	static u32 Loc_swPrev[ROWS_NUM][COLS_NUM]={0};
+	u8 Loc_swState=0;
 	static u32  Loc_swCounts[ROWS_NUM][COLS_NUM]={0};
-	u8 flag=0;
-	u8 Send_counter=0;
-	for (u8 itr1=0; itr1<ROWS_NUM && flag==0 ; itr1++)
+
+	for (u8 itr1=0; itr1<ROWS_NUM &&Loc_swState==0; itr1++)
 	{
 		GPIO_SetPinValue(KEYPAD_Configuration[itr1+COLS_NUM].port,KEYPAD_Configuration[itr1+COLS_NUM].pin,GPIO_STATE_LOW);
 		//col->i/p
-		for (u8 itr2=0; itr2<COLS_NUM; itr2++)
+		
+		for (u8 itr2=0; itr2<COLS_NUM &&Loc_swState==0; itr2++)
 		{
 			GPIO_GetPinValue(KEYPAD_Configuration[itr2].port,KEYPAD_Configuration[itr2].pin,&Loc_swcurrent);
 
-			if(Loc_swcurrent==Loc_swPrev[itr1][itr2])
+			if(Loc_swcurrent==GPIO_STATE_LOW)
 			{
 				Loc_swCounts[itr1][itr2]++;
 			}
 			else
 			{
 				Loc_swCounts[itr1][itr2]=0;
-				Loc_swPrev[itr1][itr2]=Loc_swcurrent;
 			}
 			if(Loc_swCounts[itr1][itr2]==5)
 			{
-				if(Loc_swcurrent==GPIO_STATE_LOW)
-				{
-					G_row=itr1;
-					G_col=itr2;	
-					G_swState=KEYPAD_Keys[G_row][G_col];
-					Loc_swCounts[itr1][itr2]=0;
-					//Loc_swcurrent=0;
-					flag=1;
-					break;
-				
-				}
-				else
-				{
-					
-					//G_swState=0;
-
-				}
-				
-
+				Loc_swState=KEYPAD_Keys[itr1][itr2];
+				Loc_swCounts[itr1][itr2]=0;
 			}
-			else if(Loc_swCounts[itr1][itr2]<5)
+			if(Loc_swState!=G_swState)
 			{
-				G_swState=0;
+				G_swState=Loc_swState;
+				G_newPressedKey=1;
 
 			}
-			//Loc_swPrev[itr1][itr2]=Loc_swcurrent;
-
+		
 		}
 		
 		GPIO_SetPinValue(KEYPAD_Configuration[itr1+COLS_NUM].port,KEYPAD_Configuration[itr1+COLS_NUM].pin,GPIO_STATE_HIGH);
 
 	}
-
 }
